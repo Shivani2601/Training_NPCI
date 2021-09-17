77a1d41ce1f0cbf223f9c6b554bbb6bfda3ee62f
Shivani2601 <shivanibisht2601@gmail.com>
2021-09-17 11:12:45 +0530

    Ques 1 and 2 DS

---
+++ Ques1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Ques1
+{
+    
+    public static void sortArray(int[] array)
+    {
+        int temp = 0;
+          for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    temporary = array[i];
+                    array[i] = array[j];
+                    array[j] = temporary;
+                }
+            }
+        }
+  
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = new int[] { -5, -9, 8, 12, 1, 3 };
+        sortArray(array);
+
+       
+    }
+
+
+    
+}
---
+++ Ques2.java
@@ -0,0 +1,33 @@
+public class Ques2 {
+    
+    public static void main(String[] args) {
+        int arr[]=new int[] {1,5,5,8,9};
+        int find=5;
+
+        firstOccurence(arr, find);
+        allOccurence(arr, find);
+    }
+
+    public static void firstOccurence(int arr[], int x)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i]==x)
+            {System.out.println(i);
+             return;
+           }
+        }
+        
+    }
+
+    public static void allOccurence(int arr[], int x)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i]==x)
+            {
+               System.out.println(i);
+            }
+        }
+    }
+}
